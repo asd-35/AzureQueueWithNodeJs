c613fb63bb2f0a900a795027efb6ac70f780e432
asd-35 <43975131+asd-35@users.noreply.github.com>
2020-12-28 17:08:48 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const morgan = require("morgan");
+
+
+
+
+
+app = express();
+app.set("view engine","ejs");
+
+app.get("/", (req,res) => {
+	var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: "admin363",
+        password: "Password363!",
+        server: "cmpe363-sql.database.windows.net", 
+        database: "sqldb1"
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('SELECT TOP (100) * FROM [SalesLT].[Customer]', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+           
+            res.send("<h1>Arda Saridogan</h1>" +
+	"<p>118200062</p>" + "<hr>" + "<h3> data requested in assigment </h3>" + JSON.stringify(recordset["recordsets"][0]));
+            
+        });
+    });
+	
+	});
+
+
+
+
+
+
+
+
+
+app.listen(3000);
+
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+
+
