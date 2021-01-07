fdbe39c179406cfab25fac6cb96c437a5525e65e
Arda Saridogan <saridoganarda35@gmail.com>
2021-01-08 01:05:31 +0300

    final

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
+const { DefaultAzureCredential } = require("@azure/identity");
+const { QueueServiceClient } = require("@azure/storage-queue");
 
 
 
-//dadad
 
 app = express();
 app.set("view engine","ejs");
+
+
+app.listen(process.env.PORT || 3000);
+
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req,res) => {
 	var sql = require("mssql");
@@ -28,31 +36,50 @@ app.get("/", (req,res) => {
         var request = new sql.Request();
            
         // query to the database and get the records
-        request.query('SELECT TOP (100) * FROM [SalesLT].[Customer]', function (err, recordset) {
+        request.query('SELECT TOP (10) * FROM [SalesLT].[Customer]', function (err, recordset) {
             
             if (err) console.log(err)
 
            
-            res.send("<h1>Arda Saridogan</h1>" +
-	"<p>118200062</p>" + "<hr>" + "<h3> data requested in assigment </h3>" + JSON.stringify(recordset["recordsets"][0]));
+            res.status(200).render("main",{data: JSON.stringify(recordset["recordsets"][0])});
             
         });
     });
 	
 	});
+app.post("/", (req,res) => {
+    var data = [req.body.idone,req.body.idtwo,req.body.idtri]
+    for(var i = 0; i< 3;i++)
+    {
+        let buff = new Buffer(data[i]);
+        data[i] = buff.toString('base64');
+        console.log(data[i])
+    }
+    
+      
+    const connStr = "DefaultEndpointsProtocol=https;AccountName=saforcmpe363;AccountKey=qPXc94ZUxS/ebpRjnuWLVjLbfJ++PBEQsAqsUJG+iO45gCK67oA2FX+sBtGUq3nOEWI43T7So67MwHSNdeCS8A==;EndpointSuffix=core.windows.net";
+ 
+    const queueServiceClient = QueueServiceClient.fromConnectionString(connStr);
+ 
+    
+ 
+const queueName = "queueforcustomerid";
+ 
+async function main() {
+  const queueClient = queueServiceClient.getQueueClient(queueName);
+  // Send a message into the queue using the sendMessage method.
+ 
+  for(var i = 0; i< 3;i++) var sendMessageResponse = await queueClient.sendMessage(data[i]);
+  res.send(
+    `Sent message successfully, service assigned message Id: ${sendMessageResponse.messageId}, service assigned request Id: ${sendMessageResponse.requestId}`
+  );
+    }
+ 
+    main();
+})
 
 
 
 
-
-
-
-
-
-app.listen(3000);
-
-app.use(morgan("dev"));
-app.use(express.static("public"));
-app.use(express.urlencoded({extended: true}));
 
 
